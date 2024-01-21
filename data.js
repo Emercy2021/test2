7dfc04237d010bcff58787449ab6955b8e769af4
Emercy2021 <145089445+Emercy2021@users.noreply.github.com>
2024-01-22 00:50:45 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,53 @@
+
+async function loadJsonData(url) {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok. Status code: ' + response.status);
+    }
+
+    return await response.json();
+}
+
+function toDataSet(json) {
+    let result = [];
+
+    for (let i = 0; i < json.length; i++) {
+        const quantity = json[i].quantity || 1;
+        for (let j = 0; j < quantity; j++) {
+            result.push(json[i])
+        }
+    }
+
+    return result
+}
+
+const dataSets = {
+    items: [
+        'foo',
+        'bar',
+    ],
+    coin: [
+        {title: 'Орёл',  image: '/coins/russia-100-rubles-1993-ob.png'},
+        {title: 'Решка', image: '/coins/russia-100-rubles-1993-rev.png'},
+        {title: 'Орёл',  image: '/coins/russia-100-rubles-1993-ob.png'},
+        {title: 'Решка', image: '/coins/russia-100-rubles-1993-rev.png'},
+        {title: 'Орёл',  image: '/coins/russia-100-rubles-1993-ob.png'},
+        {title: 'Решка', image: '/coins/russia-100-rubles-1993-rev.png'},
+        {title: 'Орёл',  image: '/coins/russia-100-rubles-1993-ob.png'},
+        {title: 'Решка', image: '/coins/russia-100-rubles-1993-rev.png'},
+        {title: 'Орёл',  image: '/coins/russia-100-rubles-1993-ob.png'},
+        {title: 'Решка', image: '/coins/russia-100-rubles-1993-rev.png'},
+        {title: 'Ребро', image: '/coins/coin-gurt.png'},
+    ],
+};
+
+loadJsonData('wheel-items.json')
+    .then(json => {
+        dataSets.items = toDataSet(json);
+    })
+    .catch(error => {
+        console.error(error);
+    })
+;
+
